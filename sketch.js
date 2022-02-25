8337935916104ebac9764ff0212ebe5d99a2d2a7
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-24 23:59:00 -0500

    Cleaned up code

--- sketch.js
+++ sketch.js
@@ -1,10 +1,7 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
-var distance
-
 function setup() {
   createCanvas(600, 600);
-  // PLAN: recursive quilt pattern with squares and/or circles
 }
 
 function drawBackground() {
@@ -13,24 +10,8 @@ function drawBackground() {
     for(var y = 0; y < height; y++) {
       var i = x;
       var j = y * 2;
-      stroke(dist(0,0,i,j)%255,dist(width,0,i,j)%255,dist(0,height,i,j)%255);
+      
       point(x,y);
-    }
-  }
-  var x = 0;
-  var y = 0;
-  for(var i = 0; i < 10; i++) {
-    drawSphere(width/2,height/2,50);
-  }
-}
-
-function drawSphere(xPos,yPos,radius) {
-  for(var x = xPos - radius; x < xPos + radius; x++) {
-    for(var y = yPos - radius; y < yPos + radius; y++) {
-      if(dist(xPos,yPos,x,y) <= radius) {
-        stroke((xPos - x + yPos - y) / (radius * 3) * 255);
-        point(x,y);
-      }
     }
   }
 }
