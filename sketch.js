05d23460b691acccc14df171b54cc38c302ba997
Thomas Kwashnak <shintaomonk@gmail.com>
2022-02-25 00:17:46 -0500

    experiment kindof failed ish

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
 /// <reference path="./libraries/p5.global-mode.d.ts" />
 
+class Point {
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  toString() {
+    return this.x + " " + this.y;
+  }
+}
+
+var points;
+
 function setup() {
   createCanvas(600, 600);
+  points = createPositions(width,height,10);
+
+  print(points);
 }
 
 function drawBackground() {
@@ -11,6 +27,14 @@ function drawBackground() {
       var i = x;
       var j = y * 2;
       
+      var minDist = -1;
+      points.forEach(element => {
+        var norm = dist(x,y,element.x,element.y);
+        if(minDist == -1 || norm < minDist) {
+          minDist = norm;
+        }
+      });
+      stroke((1 - minDist / width) * 255);
       point(x,y);
     }
   }
@@ -20,4 +44,13 @@ function draw() {
   background(220);
   drawBackground();
   noLoop();
+}
+
+
+function createPositions(width,height, count) {
+  var vals = [];
+  for(var i = 0; i < count; i++){
+    vals.push(new Point(Math.random() * width,Math.random() * height));
+  }
+  return vals;
 }
